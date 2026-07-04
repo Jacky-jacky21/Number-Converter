3bdc0db53b90cedc8d817ea45bb6def3e40fd14b
Jacky-jacky21 <jakobst107@gmail.com>
2026-07-05 00:21:33 +0200

    really small code clean up

--- numberconv.py
+++ numberconv.py
@@ -1,14 +1,9 @@
 import argparse
 import sys
 
-#For fast convertion without opening menu
 def dezToBin(number):
     try:
         return bin(int(number)).replace("0b", "")
     except ValueError:
         return "Error: wrong input!"
 
-
-
-#With menu 
-#coming soon...
