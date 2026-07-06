e37084a409fdbc5e8112451023fb5b241266d229
Jacky-jacky21 <jakobst107@gmail.com>
2026-07-06 22:19:41 +0200

    untested: added reworked --help and added missing fast convertions

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import numberconv
 import menu
 
 def main():
-    parser = argparse.ArgumentParser(description="Simple Number-Converter.")
-    parser.add_argument("--dectobin", metavar="<DECIMAL NUMBER>", help="Convert immediately, Decimal to Binary", type=str)
-    parser.add_argument("--bintodec", metavar="<BINARY NUMBER>", help="Convert immediately, Binary to Decimal", type=str)
-    #TODO: add all short forms for the new number systems
+    parser = argparse.ArgumentParser(description="Simple Number-System-Converter.")
+    parser.add_argument("-db", "--dectobin", metavar="<DEC>", help="Convert Decimal to Binary", type=str)
+    parser.add_argument("-dh", "--dectohex", metavar="<DEC>", help="Convert Decimal to Hexadecimal", type=str)
+    parser.add_argument("-bd", "--bintodec", metavar="<BIN>", help="Convert Binary to Decimal", type=str)
+    parser.add_argument("-bh", "--bintohex", metavar="<BIN>", help="Convert Binary to Hexadecimal", type=str)
+    parser.add_argument("-hd", "--hextodec", metavar="<HEX>", help="Convert Hexadecimal to Decimal", type=str)
+    parser.add_argument("-hb", "--hextobin", metavar="<HEX>", help="Convert Hexadecimal to Binary", type=str)
     args = parser.parse_args()
 
     #Convert immediately
@@ -14,10 +17,34 @@ def main():
     if args.dectobin:
         result = numberconv.decToBin(args.dectobin)
         print(result)
+
+    #dth
+    #TODO: Test it
+    elif args.dectohex:
+        result = numberconv.decToHex(args.dectohex)
+        print(result)
         
     #btd
     elif args.bintodec:
         result = numberconv.binToDec(args.bintodec)
+        print(result)
+
+    #bth
+    #TODO: Test it
+    elif args.bintohex:
+        result = numberconv.binToHex(args.bintohex)
+        print(result)
+
+    #htd
+    #TODO: Test it
+    elif args.hextodec:
+        result = numberconv.hexToDec(args.hextodec)
+        print(result)
+
+    #htb
+    #TODO: Test it
+    elif args.hextobin:
+        result = numberconv.hexToBin(args.hextobin)
         print(result)
 
     #Open menu
